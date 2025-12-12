1a031b239087366ab14acb51a47a37d200e0bf9c
miaoda <miaoda@baidu.com>
2025-12-12 14:31:36 +0800

    Initial miaoda project setup with Taro weapp template f0911ce659e5693b9e492193111c7ee6aea691a4 no sync

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import {Text, View} from '@tarojs/components'
+import type React from 'react'
+
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>Home Page</Text>
+    </View>
+  )
+}
+
+export default Home
